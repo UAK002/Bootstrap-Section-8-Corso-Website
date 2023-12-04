b640d63aab6cda19857031709239c9b9a4ac255c
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-12-05 01:39:55 +0530

    R01

--- 0 Day/1/js/script.js
+++ 0 Day/1/js/script.js
@@ -1,1 +1,15 @@
 // Add custom JavaScript here
+
+const useScroll = () => {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+    } else {
+      navbar.classList.remove('bg-dark');
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', useScroll);
